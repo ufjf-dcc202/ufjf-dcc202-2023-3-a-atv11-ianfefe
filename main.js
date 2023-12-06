671e1687eba203b1c43a5477f161b1144041dbdb
ianfefe <ianfelixfernandes@gmail.com>
2023-12-05 23:02:35 -0300

    atribue funcoes aos botoes e ao html

--- main.js
+++ main.js
@@ -1,9 +1,34 @@
-import { getLista, limpaLista ,adicionaNaLista} from "./lista.js"
+import { getLista, limpaLista, adicionaNaLista } from "./lista.js"
 
 const pEntrada = document.querySelector('#entrada')
 const olSaida = document.querySelector('#itens')
 const btnAdicionar = document.querySelector('#adicionar')
 const btnLimpar = document.querySelector('#limpar')
 
-btnLimpar.addEventListener('click',limparElementosDaLista)
-btnAdicionar.addEventListener('click',addElementosDaLista)
+btnLimpar.addEventListener('click', limparElementosDaLista)
+btnAdicionar.addEventListener('click', adicionaItem)
+
+function adicionaItem() {
+    const texto = pEntrada.textContent
+    adicionaNaLista(texto)
+    atualizarItemDeLista()
+    pEntrada.textContent = ''
+    pEntrada.focus()
+}
+function limparElementosDaLista() {
+    limpaLista()
+    atualizarItemDeLista()
+}
+function atualizarItemDeLista() {
+    const lista = getLista()
+    olSaida.innerHTML = ""
+
+    for (let i = 0; i < lista.length; i++) {
+
+        const item = lista[i]
+        const li = document.createElement('li')
+        li.textContent = item
+        olSaida.appendChild(li)
+    }
+}
+atualizarItemDeLista()
