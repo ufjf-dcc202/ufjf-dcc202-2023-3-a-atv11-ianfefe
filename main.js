36a9232d7f02fe30c21352c00e130069b52af00a
ianfefe <ianfelixfernandes@gmail.com>
2023-12-05 22:58:17 -0300

    cria arquivos js e constantes + click nos botoes

---
+++ main.js
@@ -0,0 +1,7 @@
+const pEntrada = document.querySelector('#entrada')
+const olSaida = document.querySelector('#itens')
+const btnAdicionar = document.querySelector('#adicionar')
+const btnLimpar = document.querySelector('#limpar')
+
+btnLimpar.addEventListener('click',limparElementosDaLista)
+btnAdicionar.addEventListener('click',addElementosDaLista)
